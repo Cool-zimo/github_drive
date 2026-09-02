2c6545ca49d3122c850220aa306b6804370f6ccd
Cool-zimo <cool-zimo@users.noreply.github.com>
2026-09-02 22:52:57 +0800

    chore: 版本号 v0.0.30

--- js/version.js
+++ js/version.js
@@ -1,6 +1,6 @@
 // 版本信息 - 由 release.py 自动生成
 const APP_VERSION = {
-    internalVersion: '29',
-    formalVersion: '0.0.29',
-    displayVersion: 'v0.0.29'
+    internalVersion: '30',
+    formalVersion: '0.0.30',
+    displayVersion: 'v0.0.30'
 };
