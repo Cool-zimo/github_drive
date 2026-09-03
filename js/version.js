4322b759b06a7cd067f24ba7f5ee01310037388f
Cool-zimo <cool-zimo@users.noreply.github.com>
2026-09-03 19:25:43 +0800

    chore: 版本号 v0.0.31 - 10个大功能合并

--- js/version.js
+++ js/version.js
@@ -1,6 +1,6 @@
 // 版本信息 - 由 release.py 自动生成
 const APP_VERSION = {
-    internalVersion: '30',
-    formalVersion: '0.0.30',
-    displayVersion: 'v0.0.30'
+    internalVersion: '31',
+    formalVersion: '0.0.31',
+    displayVersion: 'v0.0.31'
 };
